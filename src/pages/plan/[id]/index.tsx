9d65e1ad78935747952b88c2081849da9a0d622b
Jisu Sim <rlj1202@gmail.com>
2023-04-16 20:19:40 +0900

    Add: 메인 페이지 추가
    Co-authored-by: JuJinHyeong <JuJinHyeong@users.noreply.github.com>
    Co-authored-by: SangMin Shin <qilip@qilip.io>
    Co-authored-by: BEEPMan <bio8641@naver.com>

---
+++ src/pages/plan/[id]/index.tsx
@@ -0,0 +1,18 @@
+import { GetServerSideProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   context.params;
+
+//   return {
+//     props: {},
+//   };
+// };
+
+const PlanPage: NextPage = ({}) => {
+  const router = useRouter();
+
+  return <></>;
+};
+
+export default PlanPage;
