3a61fcf53fc6cd491408ad615515398d322d4b3a
현광빈 <bio8641@naver.com>
2023-04-27 20:06:55 +0900

    소셜 로그인 버튼 구성

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -18,7 +18,7 @@ const LoginPage: NextPage = ({}) => {
   return (
     <div>
       <div>로그인</div>
-      <GoogleOAuthProvider clientId="1089144693555-800hflqusq9stvt16h3qilrivop7v560.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId="your clientId">
         <GoogleLogin
           onSuccess={credentialResponse => {
             console.log(credentialResponse);
