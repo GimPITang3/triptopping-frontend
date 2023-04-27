7bae8323bd3b8e134a93462831d8dfb23344d5bc
현광빈 <bio8641@naver.com>
2023-04-27 20:05:11 +0900

    소셜 로그인 버튼 구성

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,9 @@
 import { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+
 // export const getServerSideProps: GetServerSideProps = async (context) => {
 //   context.params;
 
@@ -15,7 +18,17 @@ const LoginPage: NextPage = ({}) => {
   return (
     <div>
       <div>로그인</div>
-      <button className="btn btn-wide">구글로 로그인</button>
+      <GoogleOAuthProvider clientId="1089144693555-800hflqusq9stvt16h3qilrivop7v560.apps.googleusercontent.com">
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+      </GoogleOAuthProvider>
+      <div>회원가입</div>
     </div>
   );
 };
