99e371ea378d5b64ac7e4caec2f2972254465f08
jinhyeongju <jujh97@hanmail.net>
2023-04-27 19:19:23 +0900

    add tailwind datepicker

--- src/pages/plan/new/date.tsx
+++ src/pages/plan/new/date.tsx
@@ -1,14 +1,32 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import Datepicker from 'react-tailwindcss-datepicker';
 
-const Date: FC = () => {
+interface DateRange {
+  startDate: Date | null;
+  endDate: Date | null;
+}
+
+const DateSelector: FC = () => {
+  const [value, setValue] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+  });
+
+  const handleValueChange = (newValue: any) => {
+    console.log('newValue:', newValue);
+    setValue(newValue);
+  };
   return (
-    <div>
+    <div className="relative min-h-screen">
       <div>뒤로가기</div>
       <div>언제 떠나세요?</div>
-
       <div>몇일동안 여행하시나요?</div>
-
-      <div className="flex">
+      <Datepicker
+        value={value}
+        onChange={handleValueChange}
+        showShortcuts={true}
+      />
+      <div className="absolute bottom-1 right-1">
         <button className="btn btn-ghost">취소</button>
         <button className="btn btn-primary">다음</button>
       </div>
@@ -16,4 +34,4 @@ const Date: FC = () => {
   );
 };
 
-export default Date;
+export default DateSelector;
