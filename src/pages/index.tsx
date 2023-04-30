62e94a800d4d222689fc6a703aa9dd626a7f6474
현광빈 <bio8641@naver.com>
2023-04-30 17:57:35 +0900

    Add: 여행 계획 리스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,37 +16,18 @@ const TestComponent: FC<PropsWithChildren> = ({ children }) => {
 
 const ItineraryList: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex items-center space-x-4">
-      <div className="flex-1 min-w-0">
-        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-          도쿄 여행
-        </p>
-        <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-          D-1 | 4.17(월) - 4.21(금)
-        </p>
-      </div>
-      <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Read more
-          <svg
-            aria-hidden="true"
-            className="w-4 h-4 ml-2 -mr-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </a>
-      </div>
-    </div>
+    <a href="#" className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+        <div className="flex items-center space-x-4">
+          <div className="flex-1 min-w-0">
+            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+              도쿄 여행
+            </p>
+            <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+              D-1 | 4.17(월) - 4.21(금)
+            </p>
+          </div>
+        </div>
+      </a>
   );
 };
 
@@ -101,7 +82,7 @@ const Home = () => {
               내 여행 계획
             </h5>
             <a
-              href="#"
+              href="/plan/list"
               className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
             >
               View all
@@ -112,10 +93,10 @@ const Home = () => {
               role="list"
               className="divide-y divide-gray-200 dark:divide-gray-700"
             >
-              <li className="py-3 sm:py-4">
+              <li className="py-3 sm:py-1">
                 <ItineraryList />
               </li>
-              <li className="py-3 sm:py-4">
+              <li className="py-3 sm:py-1">
                 <ItineraryList />
               </li>
             </ul>
---
+++ src/pages/plan/list/index.tsx
@@ -0,0 +1,84 @@
+import { GetServerSideProps, NextPage } from 'next';
+import { FC, PropsWithChildren } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+import plusCircle from '../../../../public/pluscircle.svg'
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   context.params;
+
+//   return {
+//     props: {},
+//   };
+// };
+
+const ItineraryList: FC<PropsWithChildren> = ({ children }) => {
+    return (
+      <a href="#" className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+        <div className="flex items-center space-x-4">
+          <div className="flex-1 min-w-0">
+            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+              도쿄 여행
+            </p>
+            <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+              D-1 | 4.17(월) - 4.21(금)
+            </p>
+          </div>
+          <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">          
+            <div className="inline-flex rounded-md shadow-sm" role="group">
+              <button type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                수정
+              </button>
+              <button type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-red-700 focus:z-10 focus:ring-2 focus:ring-red-700 focus:text-red-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-red-500 dark:focus:text-white">
+                삭제
+              </button>
+            </div>
+          </div>
+        </div>
+      </a>
+    );
+  };
+
+const PlanPage: NextPage = ({}) => {
+  const router = useRouter();
+
+  return (
+    <div>
+        <div className="p-4 pt-8">
+        <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex items-center justify-between mb-4">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+              내 여행 계획
+            </h5>
+          </div>
+          <div className="flow-root">
+            <ul
+              role="list"
+              className="divide-y divide-gray-200 dark:divide-gray-700"
+            >
+              <li className="py-3 sm:py-1">
+                <a href="#" className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                  <div className="flex items-center space-x-4">
+                    <Image src={plusCircle} alt="#" />
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      새 여행계획을 만들어보세요
+                    </p>
+                  </div>
+                </a>
+              </li>
+              <li className="py-3 sm:py-1">
+                <ItineraryList />
+              </li>
+              <li className="py-3 sm:py-1">
+                <ItineraryList />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlanPage;
