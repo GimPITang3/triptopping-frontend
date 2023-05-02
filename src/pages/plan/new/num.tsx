db4c32f4deb1519d70ebd7a88a63972598ccfbfe
qilip <qilip@qilip.io>
2023-05-02 20:28:06 +0900

    Style: name, num 스타일링

--- src/pages/plan/new/num.tsx
+++ src/pages/plan/new/num.tsx
@@ -1,9 +1,9 @@
-import { FC, useState, useContext } from 'react';
-import plus from '../../../../public/plus.svg';
-import dash from '../../../../public/dash.svg';
+import { PlanContext } from '@/contexts';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { PlanContext } from '@/contexts';
+import { FC, useContext } from 'react';
+import dash from '../../../../public/dash.svg';
+import plus from '../../../../public/plus.svg';
 
 const Num: FC = () => {
   const router = useRouter();
@@ -19,21 +19,30 @@ const Num: FC = () => {
   };
 
   return (
-    <div className="relative min-h-screen">
-      <div>몇 명이서 여행 가시나요?</div>
-      <button className="btn btn-ghost" onClick={() => onChange(false)}>
-        <Image width={32} height={32} src={dash} alt="-" />
-      </button>
-      {plan.num}
-      <button className="btn btn-ghost" onClick={() => onChange(true)}>
-        <Image width={32} height={32} src={plus} alt="+" />
-      </button>
-      <div className="absolute bottom-1 right-1 space-x-2">
-        <button className="btn" onClick={() => router.push('/plan/new/name')}>
+    <div className="flex flex-col min-h-screen p-8">
+      <div className="font-bold text-3xl mb-8">
+        여행 계획 이름 대충 넣어줭
+      </div>
+      <div className="flex-grow">
+        <div className="text-xl my-4">총 여행 인원을 알려주세요!</div>
+        <div className="flex py-8 justify-center">
+          <button className="btn btn-outline hover:bg-slate-300" onClick={() => onChange(false)}>
+            <Image width={32} height={32} src={dash} alt="-" />
+          </button>
+          <div className="text-4xl font-bold self-center mx-16 rounded">
+            {plan.num}
+          </div>
+          <button className="btn btn-outline hover:bg-slate-300" onClick={() => onChange(true)}>
+            <Image width={32} height={32} src={plus} alt="+" />
+          </button>
+        </div>
+      </div>
+      <div className="flex w-full space-x-4">
+        <button className="flex-1 btn" onClick={() => router.push('/plan/new/name')}>
           뒤로가기
         </button>
         <button
-          className="btn btn-primary"
+          className="flex-1 btn btn-primary"
           onClick={() => router.push('/plan/new/budget')}
         >
           다음
