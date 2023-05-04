1548bc92ebcd053df0a16fc549e008c63557b997
jinhyeongju <jujh97@hanmail.net>
2023-05-04 18:36:29 +0900

    plan 타입에 맞게 변경

--- src/pages/plan/[id]/index.tsx
+++ src/pages/plan/[id]/index.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
-import { useCallback, useState } from 'react';
+import {
+  ChangeEvent,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import arrowLeftCircle from '../../../../public/arrowleftcircle.svg';
 import hamburger from '../../../../public/hamburger.svg';
 import { DateTime } from 'luxon';
@@ -11,6 +17,8 @@ import {
   Marker,
   StandaloneSearchBox,
 } from '@react-google-maps/api';
+import { PlanContext } from '@/contexts';
+import { IItinerary, IPlan } from '@/types';
 
 interface SearchResult {
   position: {
@@ -24,60 +32,138 @@ const GetItineraryValue = (itinerary: any, key: string) => {
   return itinerary.manual[key] || itinerary.system[key];
 };
 
-const initialPlan = {
-  _id: '...생략...',
+const initialPlan: IPlan = {
   name: '지수의 콩국수 여행기',
-  status: 'normal', // normal or deleted
-  numberOfMembers: 4,
+  num: 4,
   members: [],
   // 예산 설정, 단위는 원
   budget: 2000000,
   // 기간 설정
-  period: '3', // 3일, 이게 필수
-  startAt: '2024-01-01', // 이건 선택
+  period: 3, // 3일, 이게 필수
+  startAt: new Date('2024-01-01'), // 이건 선택
   tags: ['바다', '식도락', '힐링'],
   // 일정
   itineraries: [
-    {
-      type: 'place',
-      system: {
-        place: {
-          name: '인천국제공항',
+    [
+      {
+        type: 'place',
+        system: {
+          place: {
+            name: '인천국제공항',
+          },
+          time: '2023-02-28 10:00:00',
         },
-        time: '2023-02-28 10:00:00',
+        manual: {},
       },
-      manual: {},
-    },
-    {
-      type: 'place',
-      system: {
-        // 추천에 의해 결정된 내용
-        time: '2023-03-01 17:00:00',
-      },
-      manual: {
-        // 사용자에 의해 결정된 내용
-        place: {
-          name: '시부야',
+
+      {
+        type: 'place',
+        system: {
+          // 추천에 의해 결정된 내용
+          time: '2023-02-28 17:00:00',
+          place: {
+            name: '시부야',
+          },
+        },
+        manual: {
+          // 사용자에 의해 결정된 내용
         },
       },
-      cost: 1000,
-    },
-    {
-      type: 'transport',
-      // 대충 구글 API 결과 그대로 저장하기 (맵에 그릴수 있는 정도로만)
-    },
+      {
+        type: 'transport',
+        // 대충 구글 API 결과 그대로 저장하기 (맵에 그릴수 있는 정도로만)
+        system: {},
+        manual: {},
+      },
+    ],
+    [
+      {
+        type: 'place',
+        system: {
+          // 추천에 의해 결정된 내용
+          time: '2023-03-01 17:00:00',
+        },
+        manual: {
+          // 사용자에 의해 결정된 내용
+          place: {
+            name: '돈키호테',
+          },
+        },
+      },
+      {
+        type: 'place',
+        system: {
+          // 추천에 의해 결정된 내용
+          time: '2023-03-01 20:00:00',
+          place: {
+            name: '도쿄타워',
+          },
+        },
+        manual: {
+          // 사용자에 의해 결정된 내용
+        },
+      },
+      {
+        type: 'place',
+        system: {
+          // 추천에 의해 결정된 내용
+          time: '2023-03-01 22:00:00',
+          place: {
+            name: '규카츠 집',
+          },
+        },
+        manual: {
+          // 사용자에 의해 결정된 내용
+        },
+      },
+    ],
   ],
-  createdAt: '2023-03-20 20:33:56',
-  updatedAt: '2023-03-20 20:33:56',
+  createdAt: new Date('2023-03-20 20:33:56'),
+  updatedAt: new Date('2023-03-20 20:33:56'),
   deletedAt: undefined,
+};
+
+const ModifyNameModal: React.FC = () => {
+  const { plan, setPlan } = useContext(PlanContext);
+  const [name, setName] = useState(plan.name);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onClickOk = () => {
+    setPlan({ ...plan, name });
+  };
+
+  return (
+    <div>
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+      <label htmlFor="my-modal-4" className="modal cursor-pointer">
+        <label className="modal-box relative" htmlFor="">
+          <h3 className="text-lg font-bold">새로운 이름을 설정해주세요</h3>
+          <input
+            value={name}
+            className="input input-bordered py-4"
+            onChange={onChange}
+          />
+          <div>
+            <label className="btn" htmlFor="my-modal-4">
+              취소
+            </label>
+            <label className="btn" htmlFor="my-modal-4" onClick={onClickOk}>
+              확인
+            </label>
+          </div>
+        </label>
+      </label>
+    </div>
+  );
 };
 
 const PlanPage: NextPage = ({}) => {
   const router = useRouter();
   const { id } = router.query;
-  const [plan, setPlan] = useState<any>(initialPlan);
-  const start = DateTime.fromISO(plan.startAt);
-  const end = start.plus({ days: plan.period });
+  const { plan, setPlan } = useContext(PlanContext);
 
   const [map, setMap] = useState<google.maps.Map | null>(null);
   const [searchBox, setSearchBox] =
@@ -133,6 +219,10 @@ const PlanPage: NextPage = ({}) => {
     lng: -122.4194,
   };
 
+  useEffect(() => {
+    setPlan(initialPlan);
+  }, [setPlan]);
+
   return (
     <div className="min-h-screen">
       <div className="flex justify-between">
@@ -145,11 +235,24 @@ const PlanPage: NextPage = ({}) => {
       </div>
       <div>
         <div className="flex items-end">
-          <div className="text-xl font-bold">{plan.name}</div>
-          <button className="link">편집</button>
+          <div className="text-2xl font-bold">{plan.name}</div>
+          <label htmlFor="my-modal-4" className="link pl-2">
+            편집
+          </label>
         </div>
         <div>
-          {start.toISODate()}~{end.toISODate()}
+          {plan.startAt ? (
+            <div>
+              {DateTime.fromJSDate(plan.startAt).toFormat('yyyy년 MM월 dd일')} ~{' '}
+              {DateTime.fromJSDate(plan.startAt)
+                .plus({ days: plan.period })
+                .toFormat('yyyy년 MM월 dd일')}
+            </div>
+          ) : (
+            <div>
+              {plan.period}박{plan.period}일
+            </div>
+          )}
         </div>
         <div className="space-x-2">
           {plan.tags.map((tag: string, idx: number) => (
@@ -203,31 +306,36 @@ const PlanPage: NextPage = ({}) => {
         </div>
       </LoadScript>
       <div className="space-y-2 p-4">
-        <h1 className="font-bold">Day1</h1>
-        {plan.itineraries
-          .filter((it: any) => it.type === 'place')
-          .map((itinerary: any, idx: number) => (
-            <div key={`day-${idx}`}>
-              <div className="flex items-center space-x-4">
-                <div className="flex flex-col">
-                  <div />
-                  <div className="avatar placeholder">
-                    <div className="bg-neutral-focus text-neutral-content rounded-full w-6">
-                      <span className="text-l">{idx + 1}</span>
+        {plan.itineraries.map((itineraryDaily: IItinerary[], idx: number) => (
+          <div key={`day-${idx}`}>
+            <h1 className="font-bold">{`Day${idx + 1}`}</h1>
+            {itineraryDaily
+              .filter((itinerary: IItinerary) => itinerary.type === 'place')
+              .map((itinerary: IItinerary, idx: number) => (
+                <div className="py-1" key={`itinerary-${idx}`}>
+                  <div className="flex items-center space-x-4">
+                    <div className="flex flex-col">
+                      <div />
+                      <div className="avatar placeholder">
+                        <div className="bg-neutral-focus text-neutral-content rounded-full w-6">
+                          <span className="text-l">{idx + 1}</span>
+                        </div>
+                      </div>
+                      <div>100m</div>
+                    </div>
+                    <div className="card-body shadow-lg bg-[#fafcff]">
+                      <h2 className="card-title">
+                        {GetItineraryValue(itinerary, 'place').name}
+                      </h2>
+                      <p>place details</p>
                     </div>
                   </div>
-                  <div>100m</div>
                 </div>
-                <div className="card-body shadow-lg bg-[#fafcff]">
-                  <h2 className="card-title">
-                    {GetItineraryValue(itinerary, 'place').name}
-                  </h2>
-                  <p>place details</p>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
+          </div>
+        ))}
       </div>
+      <ModifyNameModal />
     </div>
   );
 };
