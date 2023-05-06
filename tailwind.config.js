644a1919a34414eae27d271740457213c5152b52
qilip <qilip@qilip.io>
2023-05-06 23:31:03 +0900

    Change: 임시 primary color 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,18 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        trip: {
+          primary: "#22d3ee",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+    ],
+  },
 };
