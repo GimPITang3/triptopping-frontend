919e29125fdf7fcc850cb790c27ad65157c3f00c
jinhyeongju <jujh97@hanmail.net>
2023-05-08 19:15:13 +0900

    장소 추가 선택 기능 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,19 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), require('tailwind-scrollbar-hide')],
   daisyui: {
     themes: [
       {
         trip: {
-          primary: "#22d3ee",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
+          primary: '#22d3ee',
+          secondary: '#f6d860',
+          accent: '#37cdbe',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
         },
       },
-      "dark",
+      'dark',
     ],
   },
 };
