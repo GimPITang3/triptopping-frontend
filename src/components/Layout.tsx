faa297f2f381db2b0a37e7e7e80010184acd2638
qilip <qilip@qilip.io>
2023-05-29 18:01:56 +0900

    Design: 레이웃 min-h-screen

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="container mx-auto max-w-screen-md bg-slate-50 text-black">
+    <div className="container mx-auto max-w-screen-md min-h-screen bg-slate-50 text-black">
       <main>{children}</main>
     </div>
   );
