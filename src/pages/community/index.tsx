6a01cbf24f5058d16c1fe5e81bad80d5819e7a63
BEEPMan <34468922+BEEPMan@users.noreply.github.com>
2023-05-31 14:55:12 +0900

    Fix: 커뮤니티 카드 너비 조정

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -96,7 +96,7 @@ const CommunityPage: NextPage = ({}) => {
                 </div>
 
                 <div className="flex items-center justify-center">
-                  <div className="grid grid-cols-2 gap-4">
+                  <div className="grid grid-cols-1 gap-4">
                     {articles.map((article) => {
                       return (
                         <CommunityCard
